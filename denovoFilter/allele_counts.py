1d7f4f72172d5b0d27677ba3f5ed7d99f9011bca
Jeremy McRae <jm33@sanger.ac.uk>
2016-04-18 15:33:30 +0100

    get new dataframes for allele counting rather than modifying original dataframe

--- denovoFilter/allele_counts.py
+++ denovoFilter/allele_counts.py
@@ -19,11 +19,12 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
+from __future__ import division
+
 import pandas
-import numpy
 
 def extract_alt_and_ref_counts(de_novos):
-    """extract ALT and REF counts of forward and reverse reads for the trio members
+    """extract ALT and REF counts of forward and reverse reads for trio members
     
     Args:
         de_novos: dataframe of de novo variants
@@ -35,57 +36,54 @@ def extract_alt_and_ref_counts(de_novos):
     
     members = ["child", "father", "mother"]
     
+    counts = de_novos[['person_stable_id', 'chrom', 'pos', 'ref', 'alt', 'symbol']].copy()
+    
     for member in members:
         dp4 = de_novos["dp4_{0}".format(member)].str.split(",")
         
         # split the forward and reverse counts by ref and alt for each member
-        de_novos["{0}_ref_F".format(member)] = [ int(x[0]) for x in dp4 ]
-        de_novos["{0}_ref_R".format(member)] = [ int(x[1]) for x in dp4 ]
-        de_novos["{0}_alt_F".format(member)] = [ int(x[2]) for x in dp4 ]
-        de_novos["{0}_alt_R".format(member)] = [ int(x[3]) for x in dp4 ]
+        counts["{0}_ref_F".format(member)] = [ int(x[0]) for x in dp4 ]
+        counts["{0}_ref_R".format(member)] = [ int(x[1]) for x in dp4 ]
+        counts["{0}_alt_F".format(member)] = [ int(x[2]) for x in dp4 ]
+        counts["{0}_alt_R".format(member)] = [ int(x[3]) for x in dp4 ]
     
     # get the minimum alternate allele count from the parents
-    alts = pandas.DataFrame({"a": de_novos["mother_alt_F"] + de_novos["mother_alt_R"],
-        "b": de_novos["father_alt_F"] + de_novos["father_alt_R"]})
-    de_novos["min_parent_alt"] = alts.min(axis=1)
+    alts = pandas.DataFrame({"a": counts[["mother_alt_F", "mother_alt_R"]].sum(axis=1),
+        "b": counts[["father_alt_F", "father_alt_R"]].sum(axis=1)})
+    counts["min_parent_alt"] = alts.min(axis=1)
     
-    return de_novos
+    return counts
 
-def count_gene_recurrence(de_novos):
-    """ count of number of sites called per gene
+def get_recurrent_genes(de_novos):
+    """ count candidate de novo mutations per gene
     
     Args:
         de_novos: dataframe of de novo variants
     
     Returns:
-        data frame of de novos, but with an extra column indicating the number
-        of candidate sites in the gene.
+        dictionary of counts indexed by HGNC symbol
     """
     
-    de_novos["key"] = de_novos["chrom"].map(str) + de_novos["pos"].map(str) + de_novos["alt"].map(str)
-    
-    # count of number times that gene is called
+    # count of number of candidates per gene
     counts = de_novos["symbol"].value_counts()
-    counts = pandas.DataFrame({"symbol": counts.index, "count.genes": counts})
-    de_novos = de_novos.merge(counts)
     
-    return de_novos
+    return counts.index[counts > 1]
 
-def get_allele_counts(vars, gene=False):
+def get_allele_counts(variants, gene=False):
     """ counts REF and ALT alleles in reads for candidate de novo events
     
     Args:
-        vars: dataframe of de novo variants, all at a single site, or within a
-             single gene.
+        variants: dataframe of de novo variants, all at a single site, or within
+            a single gene.
         gene: whether the set of variants of all within a single gene (rather
             than at a site)
     
     Returns:
-        list of forward reference, forward alternate.
+        dictionary of forward reference, forward alternate.
     """
     
-    vars = vars.drop([vars.columns[0]], axis=1)
-    counts = vars.sum()
+    variants = variants.drop([variants.columns[0]], axis=1)
+    counts = variants.sum()
     
     # count parental alt and ref
     parent_alt = sum(counts[["mother_alt_F", "father_alt_F", "mother_alt_R", "father_alt_R"]])
@@ -105,11 +103,11 @@ def get_allele_counts(vars, gene=False):
     
     return values
 
-def get_depths_and_proportions(de_novos):
+def get_depths_and_proportions(counts):
     """ get the read depths, alt depths and alt proportions for the trio members
     
     Args:
-        de_novos: pandas dataframe containing colums of child_ref_F, child alt_R
+        counts: pandas dataframe containing colums of child_ref_F, child alt_R
             etc
     
     Returns:
@@ -118,18 +116,19 @@ def get_depths_and_proportions(de_novos):
     """
     
     # get the trio depths
-    de_novos["child_depth"] = de_novos[["child_ref_F", "child_ref_R", "child_alt_F", "child_alt_R"]].sum(axis=1)
-    de_novos["father_depth"] = de_novos[["father_ref_F", "father_ref_R", "father_alt_F", "father_alt_R"]].sum(axis=1)
-    de_novos["mother_depth"] = de_novos[["mother_ref_F", "mother_ref_R", "mother_alt_F", "mother_alt_R"]].sum(axis=1)
+    child_depth = counts[["child_ref_F", "child_ref_R", "child_alt_F", "child_alt_R"]].sum(axis=1)
+    dad_depth = counts[["father_ref_F", "father_ref_R", "father_alt_F", "father_alt_R"]].sum(axis=1)
+    mom_depth = counts[["mother_ref_F", "mother_ref_R", "mother_alt_F", "mother_alt_R"]].sum(axis=1)
     
     # get the depth of the alternate alleles
-    de_novos["child_alts"] = de_novos[["child_alt_F", "child_alt_R"]].sum(axis=1)
-    de_novos["father_alts"] = de_novos[["father_alt_F", "father_alt_R"]].sum(axis=1)
-    de_novos["mother_alts"] = de_novos[["mother_alt_F", "mother_alt_R"]].sum(axis=1)
+    child_alts = counts[["child_alt_F", "child_alt_R"]].sum(axis=1)
+    dad_alts = counts[["father_alt_F", "father_alt_R"]].sum(axis=1)
+    mom_alts = counts[["mother_alt_F", "mother_alt_R"]].sum(axis=1)
     
-    # calculate the alt proportions
-    de_novos["child_prp"] = de_novos["child_alts"]/de_novos["child_depth"]
-    de_novos["mother_prp"] = de_novos["mother_alts"]/de_novos["mother_depth"]
-    de_novos["father_prp"] = de_novos["father_alts"]/de_novos["father_depth"]
+    values = pandas.DataFrame({
+        'child_depth': child_depth, 'dad_depth': dad_depth, 'mom_depth': mom_depth,
+        'child_alts': child_alts, 'dad_alts': dad_alts, 'mom_alts': mom_alts,
+        'child_prp': child_alts/child_depth, 'dad_prp': dad_alts/dad_depth,
+        'mom_prp': mom_alts/mom_depth}, index=counts.index)
     
-    return de_novos
+    return values
